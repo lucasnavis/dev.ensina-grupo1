4409e95813a741048ea6c615782b1886050441df
lucasnavis <lucassantosnavis@usp.br>
2025-10-23 20:04:07 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+Portfolio Optimization with Markowitz Model
+Dev.Ensina - Grupo 1
+15/10/2025 - xx/xx/2026?
+"""
+
+import numpy as np
+import pandas as pd
+import yfinance as yf
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Configurações de visualização
+plt.style.use('seaborn-v0_8-darkgrid')
+sns.set_palette("husl")
+
